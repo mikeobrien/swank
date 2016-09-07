f0066eaee001dcd045728fdc94c22381b45bf15b
mob <mob@mikeobrien.net>
2016-09-06 21:07:51 -0400

    Refactored overrides.

--- src/Swank/Description/OptionalAttribute.cs
+++ src/Swank/Description/OptionalAttribute.cs
@@ -4,4 +4,10 @@ namespace Swank.Description
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
     public class OptionalAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
+    public class OptionalForPostAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
+    public class OptionalForPutAttribute : Attribute { }
 }
